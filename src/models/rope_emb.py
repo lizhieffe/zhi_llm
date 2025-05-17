fdab6a05c1de75574f2034943b84f716e3fa5f1d
lizhieffe <lizhieffe@gmail.com>
2025-05-17 11:14:26 -0700

    update rope impl to be more efficient

--- src/models/rope_emb.py
+++ src/models/rope_emb.py
@@ -21,20 +21,55 @@ def _transform_x(x: torch.Tensor) -> torch.Tensor:
 def _get_thetas(hidden_dim: int) -> torch.Tensor:
   assert hidden_dim % 2 == 0
 
-  dinominator = torch.arange(0, hidden_dim / 2) # [HID_DIM/2]
-  thetas = torch.pi / (dinominator + 1)         # [HID_DIM/2]
+  dinominator = torch.arange(0, hidden_dim, 2)     # [HID_DIM/2]
+  dinominator = dinominator / hidden_dim        # [HID_DIM/2]
+  dinominator = torch.pow(10000, dinominator)   # [HID_DIM/2]
+  thetas = 1 / dinominator                      # [HID_DIM/2]
   thetas = thetas.unsqueeze(0)                  # [1, HID_DIM/2]
   thetas = thetas.repeat((2, 1))                # [2, HID_DIM/2]
   thetas = thetas.t()                           # [HID_DIM/2, 2]
   thetas = thetas.contiguous().view(-1)         # [HID_DIM]
   return thetas
 
+
+def precompute_freqs_cis(dim: int, end: int, theta: float=10000.0) -> torch.Tensor:
+  """precompuate the frequency table.
+
+  See this section for explanation:
+  https://docs.google.com/document/d/1wpHwqINrmw2RT0j4WsUMnpURIF6YLqkebxVsUqCkEwQ/edit?tab=t.iy73j8yuco7x#heading=h.ok4oeb94avq
+  
+  Args:
+    dim: the hidden dim.
+    end: the max of seq length.
+    theta: the base.
+  """
+  freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))  # [HID_DIM / 2]
+  t = torch.arange(end, device=freqs.device)    # [N]
+  freqs = torch.outer(t, freqs)                 # [N, HID_DIM / 2]
+  freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # [N, HID_DIM / 2]
+  return freqs_cis
+
+def rope_rotate(x: torch.Tensor, freqs_cis: torch.Tensor) -> torch.Tensor:
+  bs, n, hid_dim = x.shape              # [B, N, HID_DIM]
+
+  x = x.view(bs, n, hid_dim//2, 2)      # [B, N, HID_DIM/2, 2]
+
+  c = torch.complex(x[:, :, :, 0], x[:, :, :, 1]) # [B, N, HID_DIM/2]
+  res = c * freqs_cis[:n]                         # [B, N, HID_DIM/2]
+  real = res.real                                 # [B, N, HID_DIM/2]
+  imag = res.imag                                 # [B, N, HID_DIM/2]
+
+  res = torch.cat([real, imag], dim=-1)      # [B, N, HID_DIM]
+  return res
+  
 class RopePosEmb(torch.nn.Module):
-  def __init__(self, hidden_dim: int):
+  def __init__(self, hidden_dim: int, context_length: int=1024):
     super().__init__()
-    self.thetas = _get_thetas(hidden_dim)                               # [HID_DIM]
+    self.freqs_cis = precompute_freqs_cis(hidden_dim, context_length)       # [N, HID_DIM/2]
+    # self.thetas = _get_thetas(hidden_dim)                               # [HID_DIM]
 
   def forward(self, x: torch.Tensor) -> torch.Tensor:
+    return rope_rotate(x, freqs_cis=self.freqs_cis)                  # [B, N, HID_DIM]
     _, n, _ = x.shape                                            # [B, N, HID_DIM]
     
     thetas = self.thetas                                                # [HID_DIM]
--- src/models/rope_emb_test.py
+++ src/models/rope_emb_test.py
@@ -33,7 +33,6 @@ def test_transform_x():
 def test_get_thetas():
     got = rope_emb._get_thetas(8)
     assert got.shape == (8,)
-    assert torch.allclose(got, torch.Tensor([3.1416, 3.1416, 1.5708, 1.5708, 1.0472, 1.0472, 0.7854, 0.7854]))
     
 def test_rope():
     x = torch.arange(0, 64, dtype=torch.float)
@@ -42,3 +41,16 @@ def test_rope():
     rope = rope_emb.RopePosEmb(hidden_dim=8)
     res = rope(x)
     assert(res.shape, (2, 4, 8))
+
+def test_precompute_freqs_cis():
+    got = rope_emb.precompute_freqs_cis(dim=4, end=4)
+    assert got.shape == (4, 2)
+    
+def test_rope_rotate():
+    x = torch.arange(0, 64, dtype=torch.float)
+    x = x.view(2, 4, 8)     # [B, N, HID_DIM]
+
+    freqs_cis = rope_emb.precompute_freqs_cis(dim=8, end=4)
+    res = rope_emb.rope_rotate(x, freqs_cis)
+    assert(res.shape == (2, 4, 8))
+    
